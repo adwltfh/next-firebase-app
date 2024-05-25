45a569d20d8dc00601ce77fc7c25f54aee26ae86
Adawiyyah Latifah <adawiyyahlatifah@gmail.com>
2024-05-25 20:33:36 +0700

    setup firebase, add setup authentication

---
+++ app/admin/page.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import { useRouter } from "next/navigation";
+
+const Page = () => {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user == null) router.push("/");
+  }, [user]);
+
+  return <h1>Only logged in user can view this page</h1>;
+};
---
+++ firebase/firestore/addData.js
@@ -0,0 +1,19 @@
+import firebase_app from "../config";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+const db = getFirestore(firebase_app);
+const addData = async (collection, id, data) => {
+    let result = null, error = null;
+
+    try {
+        result = await setDoc(doc(db, collection, id), data, {
+            merge: true,
+        });
+    } catch (error) {
+        error = e;
+    }
+
+    return {result, error};
+}
+
+export default addData;
